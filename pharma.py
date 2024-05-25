8f795e2e454c9f05c1220bb5eb452d7942973957
ChatNoir2004 <161233439+ChatNoir2004@users.noreply.github.com>
2024-05-25 13:22:50 +0530

    Update pharma.py

--- pharma.py
+++ pharma.py
@@ -1,10 +1,6 @@
 from tkinter import*
 from PIL import Image,ImageTk
 from tkinter import ttk
-import mysql.connector
-from tkinter import messagebox
-
-
 
 class PharmacyManagementSystem:
     def __init__(self,root):
@@ -12,34 +8,16 @@ class PharmacyManagementSystem:
         self.root.title("Pharmacy Management System")
         self.root.geometry("1550x800+0+0")
 
-
-
-
-
-        # ==============addMed variable ===========
-        self.addmed_var=StringVar()
-        self.refMed_var=StringVar()
-
-        # ============== in variable ======================================
-        self.ref_var=StringVar()
-        self.cmpName_var=StringVar()
-        self.typeMed_var=StringVar()
-        self.medName_var StringVar()
-        self.lot_var=StringVar()
-        self.issuedate_var=StringVar()
-        self.expdate_var=StringVar()
-        self.uses_var=StringVar()
-        self.sideEffect_var=StringVar()
-        self.warning_var StringVar()
-        self.dosage_var=StringVar()
-        self.price_var=StringVar()
-        self.product_var=StringVar()
-        
         lbltitle=Label(self.root,text="PHARMACY MANAGEMENT SYSTEM",bd=7, relief=RIDGE
                       ,bg="white",fg="darkblue",font=("times new roman",40,"bold"),padx=3)
         
         lbltitle.pack(side=TOP,fill=X)
-        file_path = r"D:\College\Sem 4\CS6106\Project\tabletlogo.png"
+        file_path = r"C:\Pharmacy-Management-System\Pharmacy-Management-System-master\tabletlogo.png"
+        file_path2=r"C:\Pharmacy-Management-System\Pharmacy-Management-System-master\img2.jpg"
+        file_path3=r"C:\Pharmacy-Management-System\Pharmacy-Management-System-master\img3.jpg"
+        file_path4=r"C:\Pharmacy-Management-System\Pharmacy-Management-System-master\img4.jpg"
+
+        
 
         img1 = Image.open(file_path)
         img1 = img1.resize((60,60),Image.Resampling.LANCZOS)
@@ -58,9 +36,9 @@ class PharmacyManagementSystem:
         DataFrameRight.place(x=800,y=5,width=400,height=350)
         #===================buttonsFrame========================
         ButtonFrame=Frame(self.root,bd=7,relief=RIDGE,padx=10)
-        ButtonFrame.place(x=0,y=420,width=1530,height=50)
+        ButtonFrame.place(x=0,y=430,width=1530,height=50)
         #====================MainButton========================
-        btnAddData=Button(ButtonFrame,command=self.add_data, text="Medicine Add",font=("times new roman",20,"bold"),bg="darkblue",fg="white")
+        btnAddData=Button(ButtonFrame,text="Medicine Add",font=("times new roman",20,"bold"),bg="darkblue",fg="white")
         btnAddData.grid(row=0,column=0)
 
         btnAddData=Button(ButtonFrame,text="Update",font=("times new roman",20,"bold"),bg="darkblue",fg="white")
@@ -80,7 +58,7 @@ class PharmacyManagementSystem:
 
         serch_combo=ttk.Combobox(ButtonFrame,width=12,font=("times new roman",13,"bold"),state="readonly")
         serch_combo.grid(row=0,column=6)
-        serch_combo['values']=("Ref","Medname","Lot")
+        serch_combo["values"]=("Ref","Medname","Lot")
         serch_combo.grid(row=0,column=6)
         serch_combo.current(0)
 
@@ -89,23 +67,19 @@ class PharmacyManagementSystem:
 
         searchBtn=Button(ButtonFrame,text="Search",width=7,font=("times new roman",20,"bold"),bg="darkblue",fg="white")
         searchBtn.grid(row=0,column=8)
+        searchBtn.place(x=1259)
 
         showAll=Button(ButtonFrame,text="Show",width=7,font=("times new roman",20,"bold"),fg="white",bg="darkblue")
         showAll.grid(row=0,column=9)
+        showAll.place(x=1380)
 
 
         #====================label and entry====================
         lblrefno=Label(DataFrameLeft,text="Reference No",font=("times new roman",13,"bold"))
         lblrefno.grid(row=0,column=0,sticky=W)
 
-
-        conn=mysql.connector.connect(host="localhost",username="root",password="",database="")
-        my_cursor=conn.cursor()
-        my_cursor.execute("select Ref from pharma")
-        row=my_cursor.fetchall()
-
         ref_combo=ttk.Combobox(DataFrameLeft,width=23,font=("times new roman",13,"bold"),state="readonly")
-        ref_combo['values']=row
+        ref_combo["values"]=("Ref","Medname","Lot")
         ref_combo.grid(row=0,column=1)
         ref_combo.current(0)
 
@@ -119,7 +93,7 @@ class PharmacyManagementSystem:
         lblTypeofMedicine.grid(row=2,column=0,sticky=W)
 
         comTypeofMedicine=ttk.Combobox(DataFrameLeft,width=23,font=("times new roman",13,"bold"),state="readonly")
-        comTypeofMedicine['values']=("Tablet","Liquid","Capsules","Topical Medicines","Drops","Inhalse","Injection")
+        comTypeofMedicine["values"]=("Tablet","Liquid","Capsules","Topical Medicines","Drops","Inhalse","Injection")
         comTypeofMedicine.grid(row=2,column=1)
         comTypeofMedicine.current(0)
 
@@ -128,14 +102,8 @@ class PharmacyManagementSystem:
         lblMedicineName=Label(DataFrameLeft,font=("times new roman",12,"bold"),text="Medicine Name",padx=2,pady=6)
         lblMedicineName.grid(row=3,column=0,sticky=W)
 
-
-        conn=mysql.connector.connect(host="localhost",username="root",password="",database="")
-        my_cursor=conn.cursor()
-        my_cursor.execute("select MedName from pharma")
-        med=my_cursor.fetchall()
-
         comMedicineName=ttk.Combobox(DataFrameLeft,state="readonly",width=23,font=("times new roman",13,"bold"))
-        comMedicineName['value']=med
+        comMedicineName["value"]=("nice","novel")
         comMedicineName.current(0)
         comMedicineName.grid(row=3,column=1)
 
@@ -184,6 +152,135 @@ class PharmacyManagementSystem:
         lblSideEffect.grid(row=5,column=2,sticky=W)
         txtSideEffect=Entry(DataFrameLeft,font=("times new roman",12,"bold"),bg="white",bd=2,relief=RIDGE,width=29)
         txtSideEffect.grid(row=5,column=3)
+
+        #_______VKC_______#
+        #==========Images=============#
+        lblhome=Label(DataFrameLeft,font=("arial",15,"bold"),text="Stay SANK Stay safe:",padx=2,pady=6,bg="white",fg="red",width=30)
+        lblhome.place(x=365,y=210)
+        
+  
+
+      # ==================== dataframeRight======================
+
+        DataFrameRight=LabelFrame(DataFrame,bd=7,relief=RIDGE,padx=20,text="Medicine Add Department",fg="darkblue",font=("times new roman",20,"bold"))
+        DataFrameRight.place(x=800,y=5,width=700,height=350)
+    
+        img2 = Image.open(file_path2)
+        img2 = img2.resize((150,120),Image.Resampling.LANCZOS)
+        self.photoimg2=ImageTk.PhotoImage(img2)
+        b1=Button(self.root,image=self.photoimg2,borderwidth=0)
+        b1.place(x=1330,y=170)
+    
+        img4 = Image.open(file_path4)
+        img4 = img4.resize((150,120),Image.Resampling.LANCZOS)
+        self.photoimg4=ImageTk.PhotoImage(img4)
+        b1=Button(self.root,image=self.photoimg4,borderwidth=0)
+        b1.place(x=1330,y=300) 
+    
+        lblrefno=Label(DataFrameRight,font=("times new roman",12,"bold"),text="Reference No:")
+        lblrefno.place(x=0,y=10)
+        txtrefno=Entry(DataFrameRight,font=("times new roman",12,"bold"),bg="white",bd=2,relief=RIDGE,width=20)
+        txtrefno.place(x=135,y=10)
+    
+        lblmedName=Label(DataFrameRight,font=("times new roman",12,"bold"),text="Medicine Name:")
+        lblmedName.place(x=0,y=45)
+        txtmedName=Entry(DataFrameRight,font=("times new roman",12,"bold"),bg="white",bd=2,relief=RIDGE,width=20)
+        txtmedName.place(x=135,y=45)
+    
+    #==================side frame==================
+
+        side_frame=Frame(DataFrameRight,bd=4,relief=RIDGE,bg="white")
+        side_frame.place(x=0,y=90,width=300,height=170)
+    
+        sc_x=ttk.Scrollbar(side_frame,orient=HORIZONTAL)
+        sc_x.pack(side=BOTTOM,fill=X)
+        sc_y=ttk.Scrollbar(side_frame,orient=VERTICAL)
+        sc_y.pack(side=RIGHT,fill=Y)
+
+        self.medicine_table=ttk.Treeview(side_frame,column=("ref","medname"),xscrollcommand=sc_x.set,yscrollcommand=sc_y.set)
+
+        sc_x.config(command=self.medicine_table.xview)  
+        sc_y.config(command=self.medicine_table.yview)
+
+        self.medicine_table.heading("ref",text="Ref")
+        self.medicine_table.heading("medname",text="Medicine Name")
+
+        self.medicine_table["show"]="headings"
+        self.medicine_table.pack(fill=BOTH,expand=1)
+
+        self.medicine_table.column("ref",width=100)
+        self.medicine_table.column("medname",width=100)
+
+        # ============ Medicine Add Buttons===================
+        down_frame=Frame(DataFrameRight,bd=4,relief=RIDGE,bg="darkblue")
+        down_frame.place(x=330,y=10,width=138,height=230)
+
+        btnAddmed=Button(down_frame,text="ADD",font=("times new roman",20,"bold"),width=8,bg="#00008B",fg="white",pady=2)
+        btnAddmed.grid(row=0,column=0)
+
+        btnAddmed=Button(down_frame,text="UPDATE",font=("times new roman",20,"bold"),width=8,bg="#0000CD",fg="white",pady=2)
+        btnAddmed.grid(row=1,column=0)
+
+        btnAddmed=Button(down_frame,text="DELETE",font=("times new roman",20,"bold"),width=8,bg="#00008B",fg="white",pady=2)
+        btnAddmed.grid(row=2,column=0)
+
+        btnAddmed=Button(down_frame,text="CLEAR",font=("times new roman",20,"bold"),width=8,bg="#0000CD",fg="white",pady=2)
+        btnAddmed.grid(row=3,column=0)
+
+       #==========Frame details=================
+        Framedetails=Frame(self.root,bd=15,relief=RIDGE)
+        Framedetails.place(x=0,y=510,width=1530,height=250)
+
+    #===========Main Table & scrollbar============
+        Table_frame=Frame(self.root,bd=15,relief=RIDGE)
+        Table_frame.place(x=10,y=510,width=1530,height=250)
+
+        scroll_x=ttk.Scrollbar(Table_frame,orient=HORIZONTAL)
+        scroll_x.pack(side=BOTTOM,fill=X)
+        scroll_y=ttk.Scrollbar(Table_frame,orient=VERTICAL)
+        scroll_y.pack(side=RIGHT,fill=Y)
+
+        self.pharmacy_table=ttk.Treeview(Table_frame,column=("ref","companyname","type","tabletname","lotno","issuedate",
+                                                             "expdate","uses","sideeffect","warning","dosage","price","productqt")
+                                                             ,xscrollcommand=scroll_x.set,yscrollcommand=scroll_y.set)
+
+        scroll_x.pack(side=BOTTOM,fill=X)
+        scroll_y.pack(side=RIGHT,fill=Y)
+
+        scroll_x.config(command=self.pharmacy_table.xview)
+        scroll_y.config(command=self.pharmacy_table.yview) 
+
+        self.pharmacy_table["show"]="headings"
+
+        self.pharmacy_table.heading("ref",text="Reference No")
+        self.pharmacy_table.heading("companyname",text="Company Name")
+        self.pharmacy_table.heading("type",text="Type of Medicine")
+        self.pharmacy_table.heading("tabletname",text="Tablet Name")
+        self.pharmacy_table.heading("lotno",text="Lot No")
+        self.pharmacy_table.heading("issuedate",text="Issue Date")
+        self.pharmacy_table.heading("expdate",text="Exp Date")
+        self.pharmacy_table.heading("uses",text="Uses")
+        self.pharmacy_table.heading("sideeffect",text="Side Effect")
+        self.pharmacy_table.heading("warning",text="Prec&Warning")
+        self.pharmacy_table.heading("dosage",text="Dosage")
+        self.pharmacy_table.heading("price",text="Price")
+        self.pharmacy_table.heading("productqt",text="Product Qts")
+        self.pharmacy_table.pack(fill=BOTH,expand=1)
+
+        self.pharmacy_table.column("ref",width=100)
+        self.pharmacy_table.column("companyname",width=100)
+        self.pharmacy_table.column("type",width=100)
+        self.pharmacy_table.column("tabletname",width=100)
+        self.pharmacy_table.column("lotno",width=100)
+        self.pharmacy_table.column("issuedate",width=100)
+        self.pharmacy_table.column("expdate",width=100)
+        self.pharmacy_table.column("uses",width=100)
+        self.pharmacy_table.column("sideeffect",width=100)
+        self.pharmacy_table.column("warning",width=100)
+        self.pharmacy_table.column("dosage",width=100)
+        self.pharmacy_table.column("price",width=100)
+        self.pharmacy_table.column("productqt",width=100)
+        #_________VKC_end__________#
 
 
 #shruthi
